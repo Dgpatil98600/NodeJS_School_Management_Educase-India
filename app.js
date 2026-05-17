46c1f4a6dfce679777d7d440b0ca04b2588bf2a6
darspatil <darshan.22310901@viit.ac.in>
2026-05-17 20:18:33 +0530

    minor changes

--- app.js
+++ app.js
@@ -14,12 +14,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Root route
 app.get('/', (req, res) => {
   res.json({
     message: 'School Management API Running',
